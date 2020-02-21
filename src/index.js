8e274b05411ebb67dcd9abbfd8deef47755c1cd2
Angelo Ashmore <angelo.ashmore@walltowall.com>
2020-02-20 16:30:41 -1000

    feat: add breaks prop

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import remark from 'remark'
-import remarkBreaks from 'remark-breaks'
 import remarkReact from 'remark-react'
+import remarkBreaks from 'remark-breaks'
 
 export const MarkdownRenderer = ({
   markdown,
   components = {},
-  componentOverrides,
+  componentOverrides = {},
+  breaks = true,
 }) => {
-  const resolvedComponents = componentOverrides
-    ? Object.keys(componentOverrides).reduce((acc, key) => {
-        const Comp =
-          components[key] || (props => React.createElement(key, props))
+  const resolvedComponents = useMemo(
+    () =>
+      Object.keys(componentOverrides).reduce(
+        (acc, key) => {
+          const Comp =
+            components[key] || (props => React.createElement(key, props))
 
-        acc[key] = componentOverrides[key](Comp)
+          acc[key] = componentOverrides[key](Comp)
 
-        return acc
-      }, components)
-    : { ...components }
+          return acc
+        },
+        { ...components }
+      ),
+    [components, componentOverrides]
+  )
 
-  return remark()
-    .use(remarkReact, {
+  const instance = useMemo(() => {
+    const r = remark().use(remarkReact, {
       remarkReactComponents: resolvedComponents,
     })
-    .use(remarkBreaks)
-    .processSync(markdown).contents
+
+    if (breaks) r.use(remarkBreaks)
+
+    return r
+  }, [breaks, resolvedComponents])
+
+  return useMemo(() => instance.processSync(markdown).contents, [
+    instance,
+    markdown,
+  ])
 }
 
 export default MarkdownRenderer
